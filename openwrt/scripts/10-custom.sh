9fa29f53bca4bb86f7ad32c74d8a3f9deacce93e
oppen321 <zj18139624826@gmail.com>
2024-12-26 12:01:26 +0800

    Update 10-custom.sh

--- openwrt/scripts/10-custom.sh
+++ openwrt/scripts/10-custom.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-# 自定义脚本
+# 修改名称
+sed -i 's/OpenWrt/ZeroWrt/' package/base-files/files/bin/config_generate
+
+# ZeroWrt选项菜单
+mkdir -p files/bin
+# cp -f $GITHUB_WORKSPACE/Diy/ZeroWrt files/bin/ZeroWrt
+curl -L -o files/bin/ZeroWrt https://git.kejizero.online/zhao/files/raw/branch/main/bin/ZeroWrt
+chmod +x files/bin/ZeroWrt
+mkdir -p files/root
+curl -L -o files/root/version.txt https://git.kejizero.online/zhao/files/raw/branch/main/bin/version.txt
+chmod +x files/root/version.txt
+
+#
