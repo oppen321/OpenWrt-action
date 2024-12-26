1114d803643ce643aff2f5d7cd3173db6746e0b2
oppen321 <zj18139624826@gmail.com>
2024-12-26 19:06:17 +0800

    Update 10-custom.sh

--- openwrt/scripts/10-custom.sh
+++ openwrt/scripts/10-custom.sh
@@ -3,6 +3,10 @@
 # 修改名称
 sed -i 's/OpenWrt/ZeroWrt/' package/base-files/files/bin/config_generate
 
+# 更换AD核心
+rm -rf feeds/packages/net/adguardhome
+git clone https://git.kejizero.online/zhao/adguardhome.git package/new/adguardhome
+
 # ZeroWrt选项菜单
 mkdir -p files/bin
 # cp -f $GITHUB_WORKSPACE/Diy/ZeroWrt files/bin/ZeroWrt
