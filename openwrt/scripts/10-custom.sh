c318bc0077cad31da874cf91bddc44834b0fc4c7
oppen321 <zj18139624826@gmail.com>
2024-12-29 18:23:58 +0800

    Update 10-custom.sh

--- openwrt/scripts/10-custom.sh
+++ openwrt/scripts/10-custom.sh
@@ -32,47 +32,15 @@ function git_sparse_clone() {
 git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
 git_sparse_clone main https://github.com/linkease/istore luci
 
-# 函数：根据设备类型确定架构
-get_arch() {
-    case "$1" in
-        "x86_64")
-            echo "amd64"
-            ;;
-        "armv8"|"nanopi-r4s"|"nanopi-r5s")
-            echo "arm64"
-            ;;
-        "netgear_r8500")
-            echo "armv7"
-            ;;
-        *)
-            echo "未知设备类型: $1" >&2
-            exit 1
-            ;;
-    esac
-}
-
-# 获取设备架构
-ARCH=$(get_arch "$1")
-
-# 创建核心目录
+# 加载核心
 mkdir -p files/etc/openclash/core
 
-# 下载对应架构的核心
-CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-${ARCH}.tar.gz"
-CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/master/premium\?ref\=core | grep download_url | grep ${ARCH} | awk -F '"' '{print $4}' | grep -v 'v3')
-CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-${ARCH}.tar.gz"
+CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-${1}.tar.gz"
 GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
 GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
 
-# 下载核心文件
-echo "正在下载 ${ARCH} 架构的核心文件..."
-wget -qO- $CLASH_DEV_URL | tar xOvz > files/etc/openclash/core/clash
-wget -qO- $CLASH_TUN_URL | gunzip -c > files/etc/openclash/core/clash_tun
 wget -qO- $CLASH_META_URL | tar xOvz > files/etc/openclash/core/clash_meta
 wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
 wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
 
-# 设置执行权限
 chmod +x files/etc/openclash/core/clash*
-
-echo "核心文件下载完成！"
